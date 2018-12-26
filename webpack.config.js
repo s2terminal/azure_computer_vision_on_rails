bd25b5cf810f187e5c664f95bb6511a3034714b3
suzuki.sh <suzuki.sh@s2terminal.com>
2018-12-26 22:03:55 +0900

    fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,58 +4,58 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				include: [path.resolve(__dirname, 'app', 'assets', 'javascripts')],
-				loader: 'babel-loader',
-				options: {
-					plugins: ['syntax-dynamic-import'],
-					presets: [
-						[
-							'@babel/preset-env',
-							{ modules: false }
-						]
-					]
-				},
-				test: /\.js$/
-			},
-			{
-				test: /\.(scss|css)$/,
-				use: [
-					{ loader: 'style-loader' },
-					{ loader: 'css-loader' },
-					{ loader: 'sass-loader' }
-				]
-			}
-		]
-	},
+  module: {
+    rules: [
+      {
+        include: [path.resolve(__dirname, 'app', 'assets', 'javascripts')],
+        loader: 'babel-loader',
+        options: {
+          plugins: ['syntax-dynamic-import'],
+          presets: [
+            [
+              '@babel/preset-env',
+              { modules: false }
+            ]
+          ]
+        },
+        test: /\.js$/
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  },
 
-	entry: './app/assets/javascripts/application.js',
+  entry: './app/assets/javascripts/application.js',
 
-	output: {
-		// filename: '[name].[chunkhash].js',
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'public')
-	},
+  output: {
+    // filename: '[name].[chunkhash].js',
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public')
+  },
 
-	mode: 'development',
-	devtool: '#inline-source-map',
+  mode: 'development',
+  devtool: '#inline-source-map',
 
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	},
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  },
 
   resolve: {
     alias: {
