af4cc14370b187b625de6ab3e73f71b1f8479156
suzuki.sh <suzuki.sh@s2terminal.com>
2018-12-30 18:40:48 +0900

    use webpack-manifest-plugin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  MANIFEST_PATH = 'public/packs/manifest.json'.freeze
+
+  def asset_bundle_path(entry, **options)
+    @manifest ||= JSON.parse(File.read(MANIFEST_PATH))
+    asset_path(@manifest.fetch(entry), **options)
+  end
 end
