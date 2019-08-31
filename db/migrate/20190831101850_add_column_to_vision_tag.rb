b13b1f2105b05eaad441d438c5ea6120dc56ca95
suzuki.sh <s2s@live.jp>
2019-08-31 19:21:54 +0900

    default true

--- db/migrate/20190831101850_add_column_to_vision_tag.rb
+++ db/migrate/20190831101850_add_column_to_vision_tag.rb
@@ -1,5 +1,5 @@
 class AddColumnToVisionTag < ActiveRecord::Migration[5.2]
   def change
-    add_column :vision_tags, :active, :boolean
+    add_column :vision_tags, :active, :boolean, default: true
   end
 end
