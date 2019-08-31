5e2666991e80d17944a625aa41f96add891b8839
suzuki.sh <s2s@live.jp>
2019-08-31 16:29:52 +0900

    fix React error

--- app/assets/javascripts/application.tsx
+++ app/assets/javascripts/application.tsx
@@ -21,22 +21,22 @@ $.ajax({
   console.log(vision);
   const element = (
     <>
-      <img alt={vision.image_url} src={vision.image_url} class="vision_image" />
+      <img alt={vision.image_url} src={vision.image_url} className="vision_image" />
       <label>English Tags</label>
-      <div class="input-group">
-        <input class="form-control" id='english_tags' value={vision.vision_tags.map(function(tag){ return '#'+tag.name.replace(/\s+/g,''); }).join(' ')} />
-        <div class="btn btn-primary" onClick={()=>{copy('english_tags')}}>copy</div>
+      <div className="input-group">
+        <input className="form-control" id='english_tags' value={vision.vision_tags.map(function(tag){ return '#'+tag.name.replace(/\s+/g,''); }).join(' ')} />
+        <div className="btn btn-primary" onClick={()=>{copy('english_tags')}}>copy</div>
       </div>
       <label>Japanese Tags</label>
-      <div class="input-group">
-        <input class="form-control" id='japanese_tags' value={vision.vision_tags.map(function(tag){ return '#'+tag.translated_name.replace(/\s+/g,''); }).join(' ')} />
-        <div class="btn btn-primary" onClick={()=>{copy('japanese_tags')}}>copy</div>
+      <div className="input-group">
+        <input className="form-control" id='japanese_tags' value={vision.vision_tags.map(function(tag){ return '#'+tag.translated_name.replace(/\s+/g,''); }).join(' ')} />
+        <div className="btn btn-primary" onClick={()=>{copy('japanese_tags')}}>copy</div>
       </div>
 
       <label>Detail</label>
       <ul>
         {vision.vision_tags.map((tag) => {
-         return (<li>
+         return (<li key={tag.id}>
             { tag.name }
             ({ tag.translated_name })
             { Math.round(tag.confidence * 10000) / 100 }%
