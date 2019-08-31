ea09f66e599a09c07e9b0020facd30b586617887
suzuki.sh <s2s@live.jp>
2019-08-31 19:03:59 +0900

    active tag flag を変更可能に

--- app/assets/javascripts/components/tags.tsx
+++ app/assets/javascripts/components/tags.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const copy = (id: string) :void => {
   var elem = document.getElementById(id);
@@ -21,30 +21,56 @@ type Vision = {
   }[];
 }
 
+type ActiveTags = { [id: number]: boolean; };
+
 export default (props: TagProps) => {
   const vision = props.vision;
+
+  const [activeTags, setActiveTags] = useState(() => {
+    const defaultActiveTags: ActiveTags = {};
+    vision.vision_tags.forEach(tag => {
+      defaultActiveTags[tag.id] = true;
+    });
+    return defaultActiveTags;
+  });
+
   return (
     <>
       <img alt={vision.image_url} src={vision.image_url} className="vision_image" />
       <label>English Tags</label>
       <div className="input-group">
-        <input className="form-control" id='english_tags' value={vision.vision_tags.map((tag) => ( '#'+tag.name.replace(/\s+/g,'') )).join(' ')} />
+        <input onChange={()=>{}} className="form-control" id='english_tags' value={vision.vision_tags.filter((tag) => (activeTags[tag.id])).map((tag) => ( '#'+tag.name.replace(/\s+/g,'') )).join(' ')} />
         <div className="btn btn-primary" onClick={()=>{copy('english_tags')}}>copy</div>
       </div>
       <label>Japanese Tags</label>
       <div className="input-group">
-        <input className="form-control" id='japanese_tags' value={vision.vision_tags.map((tag) => ( '#'+tag.translated_name.replace(/\s+/g,'') )).join(' ')} />
+        <input onChange={()=>{}} className="form-control" id='japanese_tags' value={vision.vision_tags.filter((tag) => (activeTags[tag.id])).map((tag) => ( '#'+tag.translated_name.replace(/\s+/g,'') )).join(' ')} />
         <div className="btn btn-primary" onClick={()=>{copy('japanese_tags')}}>copy</div>
       </div>
 
       <label>Detail</label>
-      <ul>
+      <ul style={{listStyle: "none"}}>
         {vision.vision_tags.map((tag) => {
-          return (<li key={tag.id}>
-            { tag.name }
-            ({ tag.translated_name })
-            { Math.round(tag.confidence * 10000) / 100 }%
-          </li>);
+          return (
+            <li key={tag.id}>
+              <label>
+                <input
+                  name={`active-tag-${tag.id}`}
+                  type="checkbox"
+                  checked={ activeTags[tag.id] }
+                  onChange={() => setActiveTags(actives => {
+                    Object.keys(actives).forEach(id => {
+                      if (id != tag.id.toString()) { return; }
+                      actives[tag.id] = !actives[tag.id];
+                    });
+                    return Object.assign({}, actives);
+                  })}
+                />
+                { tag.name }
+                ({ tag.translated_name })
+                { Math.round(tag.confidence * 10000) / 100 }%
+              </label>
+            </li>);
         })}
       </ul>
     </>
